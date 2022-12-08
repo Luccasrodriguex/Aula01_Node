68219052a6a93ba956ec809301db065d6b95f4c1
J. Lucas Rodrigues <114673744+Luccasrodriguex@users.noreply.github.com>
2022-12-08 09:49:04 -0300

    Delete aula03.js

--- aula03.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = require('url');
-
-http.createServer(function(req, res){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        var q = url.parse(req.url, true).query;
-        var txt = "dia: " +q.dia+ "<br> mês: " +q.mês+ "<br> ano: " +q.ano;
-        res.end(txt);
-    }).listen(80);
